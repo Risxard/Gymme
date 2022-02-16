82590dde1e7be20d2798a19465a0723c6d38fccc
Risxard <richadson@ymail.com>
2022-02-16 13:47:50 -0300

    1.5

---
+++ JS/script-slider.js
@@ -0,0 +1,9 @@
+if ($("#img3").ready("slick-center")) {
+    $("#img1").addClass("clip-path");
+    $("#img5").addClass("clip-path");
+  }
+
+  if ($("#img2").replace("slick-center")) {
+    $("#img1").removeClass("clip-path");
+    $("#img5").removeClass("clip-path");
+  }
